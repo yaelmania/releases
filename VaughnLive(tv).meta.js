f3834006d33aa41a35c138e24c58d990a39fa94a
yaelmania <yaelmania@gmx.com>
2025-08-15 18:17:21 -0600

    Update VaughnLive(tv).meta.js

--- VaughnLive(tv).meta.js
+++ VaughnLive(tv).meta.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name        VaughnLive.tv
-// @namespace   Clean VaughnLive to see only player, no more crap else, also add-on from me web page http://yaelmania.co.nf
+// @namespace   Clean VaughnLive to see only player, no more crap else, also add-on from me web page http://yaelmania.c1.biz
 // @run-at      document-end
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @updateURL 	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).meta.js
 // @downloadURL	https://raw.githubusercontent.com/yaemania/releases/master/VaughnLive(tv).user.js
+// @icon        https://github.com/yaelmania/releases/raw/refs/heads/master/favicon03.ico
 // @include     *://vaughn*
+// @include     *://vaughn.*
 // @exclude     *://vaughn*/*chat*
-// @version     2.4.4
-// @dateLast  	2021.04.02
+// @version     2.7.8
+// @dateLast  	2025.08.14
 // @grant       none
 // ==/UserScript==
